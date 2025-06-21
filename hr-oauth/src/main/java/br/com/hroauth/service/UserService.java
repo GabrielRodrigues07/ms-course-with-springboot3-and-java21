8dbd6caba86d75f885e1c6d3a4632ceeff630f9c
Gabriel <grodrigues256@gmail.com>
2025-06-21 11:42:03 -0300

    feat(S04-#2025-06-21) Login, JWT token

--- hr-oauth/src/main/java/br/com/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/br/com/hroauth/service/UserService.java
@@ -5,12 +5,15 @@ import br.com.hroauth.model.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
@@ -21,9 +24,14 @@ public class UserService {
         User user = userFeignClient.findByEmail(email).getBody();
         if (Objects.isNull(user)) {
             LOGGER.error("Email not found: " + email);
-            throw new IllegalArgumentException("Email not found");
+            throw new UsernameNotFoundException("Email not found");
         }
         LOGGER.info("Email found: " + email);
         return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return findByEmail(username);
     }
 }
